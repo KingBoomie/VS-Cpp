62d282574082e76a8fb67df754226d50b9a7b936
Kristjan Laht <laht.kristjan@gmail.com>
2014-09-12 00:00:33 +0300

    Added time measuring function

--- Prog1/Prog1.cpp
+++ Prog1/Prog1.cpp
@@ -2,15 +2,28 @@
 #include "functions.h"
 
 using namespace std;
+//
+float time(void(*f)(int)){
+	clock_t time = clock(); //get initial value of time
+	
+	(*f)(10000); //execute function in arg[0] with argument of it's own: 10000
 
+	time = clock() - time;
+	return (float)time / CLOCKS_PER_SEC;
+}
+
+void printNums(int count) {
+	vector<int> nums = getRand(count, 1, 1000);
+
+	for (auto i : nums){
+		cout << i << "\n";
+	}
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	vector<int> nums = getRand(5, 4, 10, 1);
-
-	for (auto i : nums){
-		cout << i << " ";
-	}
+	cout << "Time: " << time(printNums) << " seconds.\n";
+	
 	return 0;
 }
 
